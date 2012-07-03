b3d93a5983964d036b48bafee4a479b26a9d6c43
Ricardo Am?zquita <ramezquitao@gmail.com>
2012-07-02 19:03:50 -0500

    Removed some things that should not be included in the 1.0 release

--- pyoptools/wavefront/field/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from field import Field
-from fields import plane_wave,spherical_wave
-__all__=["Field",
-         "plane_wave",
-         "spherical_wave"
-        ]
--- pyoptools/wavefront/psurfrep/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from psurfrep import *
-
-__all__=["PSurf"]
