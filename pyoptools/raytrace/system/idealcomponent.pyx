8a58a143f72f3c097c91126631c07a714c89e3e5
Ricardo Amézquita-Orozco <ramezquitao@cihologramas.com>
2017-07-31 16:56:50 -0500

    Se adicionó la opcion de trazo de rayos completo o trazo abreviado . En el trazo completo s emuestra como se va de las entrada a un plano principal, al otro plano principal y luego a la salida En el abreviado, se muestra el rayo a la entrada, de ahi a la salida y de ahi continua. El trazo entre entrada y salida no es valido despues de la salida si.

--- pyoptools/raytrace/system/idealcomponent.pyx
+++ pyoptools/raytrace/system/idealcomponent.pyx
@@ -1,22 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
-# cython: profile=True
-
-from numpy import inf, asarray, pi, alltrue, sometrue, isinf, isnan,array, dot, float64
+from numpy import asarray
 cimport numpy as np
 from pyoptools.raytrace.component import Component
 from pyoptools.raytrace.system import System
 from pyoptools.raytrace.ray.ray cimport Ray
 from pyoptools.raytrace.surface.plane cimport Plane
-
-from pyoptools.misc.picklable.picklable cimport Picklable
-
-#from misc import rot_mat, rot_mat_i
-
-from pyoptools.misc.cmisc.cmisc cimport * # This might be changed to a cimport. need to check
-
-from pyoptools.misc.plist.plist cimport plist
 
 class IdealThickLens(System):
     """
@@ -28,21 +15,24 @@ class IdealThickLens(System):
     principal plane 2 -> exit
     fl -> focal lenght
     
-    **ARGUMENTS**
-    
-    ===== ======================================================================
-    shape Shape of the lens (Entrance and exit surface's shape).
-    th    Thinckness of the lens (Distance between the entrance and exit
-          surfaces)
-    ppp   Tuple with the principal planes position. The position of each 
-          principal plane is measured from its corresponding entrance surface. 
-    ===== ======================================================================
+    **ARGUMENTS**    
+    ============== ===============================================================
+    shape          Shape of the lens (Entrance and exit surface's shape).
+    thickness      Thinckness of the lens (Distance between the entrance and exit
+                   surfaces)
+    princ_planes   Tuple with the principal planes position. The position of each 
+                   principal plane is measured from its corresponding entrance 
+                   surface. 
+    complete_trace If set to false the trace between the principal rays will not
+                   be shown. Still the trace from and to the entrace and exit 
+                   surfaces will be exact 
+    ============== ===============================================================
           
     The origin of the component is located in the midle point between the 
     entrance and exit surface
     """
 
-    def __init__(self,shape,th, pplanes =(0.,0.),f=100):
+    def __init__(self,shape,thickness, princ_planes =(0.,0.),f=100,complete_trace=False):
 
         System.__init__(self,n=1)
 
@@ -56,14 +46,13 @@ class IdealThickLens(System):
         self.__C3__ = Component(surflist = [(self.__H1__,(0,0,0),(0,0,0))])
         self.__C4__ = Component(surflist = [(self.__H2__,(0,0,0),(0,0,0))])
 
-        self.complist["C1"]=(self.__C1__,(0,0,-th/2.),(0,0,0))
-        self.complist["C2"]=(self.__C2__,(0,0,th/2.),(0,0,0))
-        self.complist["H1"]=(self.__C3__,(0,0,-th/2.+pplanes[0]),(0,0,0))
-        self.complist["H2"]=(self.__C2__,(0,0,th/2.+pplanes[1]),(0,0,0))
+        self.complist["E1"]=(self.__C1__,(0,0,-thickness/2.),(0,0,0))
+        self.complist["E2"]=(self.__C2__,(0,0,thickness/2.),(0,0,0))
+        self.complist["H1"]=(self.__C3__,(0,0,-thickness/2.+princ_planes[0]),(0,0,0))
+        self.complist["H2"]=(self.__C2__,(0,0,thickness/2.+princ_planes[1]),(0,0,0))
 
-        self.pplanes = pplanes
         self.f=f
-
+        self.complete_trace = complete_trace
 
     def propagate_ray(self,Ray ri):
 
@@ -79,92 +68,137 @@ class IdealThickLens(System):
         #Verificar si entra por E1 o E2
         if S==self.__E1__:
             ##Codigo a usar si el rayo entra por E1
+            print "Entrando por E1"
+            #Encontrar el punto de corte en E1
+            C,P,D = self.complist["E1"]
+            R=ri.ch_coord_sys(P,D)
+            PI=self.__E1__._intersection(R)
+
+            #Generar el rayo que va de E1 a H1
+            if self.complist["E1"][1][2]<self.complist["H1"][1][2]:
+                R = Ray(pos = PI, dir = R.dir,wavelength = wav, n=n)
+            else:
+                R = Ray(pos = PI, dir = -R.dir,wavelength = wav, n=n)
+            R_E1 = R.ch_coord_sys_inv(P,D)
+
             #Propagar hasta  H1
             C,P,D =self.complist["H1"]
-            #Cambiando el rayo al systema de coordenadas de la superficie E1, que
-            #en este caso en particular es el mismo de la componente C1
-            R=ri.ch_coord_sys(P,D)
+            R=R_E1.ch_coord_sys(P,D)
             #Calcular el punto de corte con H1 sin verificar apertura
             PI=self.__H1__._intersection(R)
             #Crear el rayo que va entre H1 y H2
             if self.complist["H1"][1][2] < self.complist["H2"][1][2]:
-                R1=Ray(pos=PI, dir=(0,0,1), wavelength=wav, n=n)
+                R=Ray(pos=PI, dir=(0,0,1), wavelength=wav, n=n)
             else:
-                R1=Ray(pos=PI, dir=(0,0,-1), wavelength=wav, n=n)
-            R2=R1.ch_coord_sys_inv(P,D)
-            ri.add_child(R2)
+                R=Ray(pos=PI, dir=(0,0,-1), wavelength=wav, n=n)
+            R_H1=R.ch_coord_sys_inv(P,D)
 
             #Propagar hasta H2
             C,P,D =self.complist["H2"]
-            R=R2.ch_coord_sys(P,D)
+            R=R_H1.ch_coord_sys(P,D)
             PI=self.__H2__._intersection(R) #No se verifica apertura
 
             #### Calculate the reffraction in H2
-            rx,ry,rz = ri.dir
-            FP=ri.dir*self.f/abs(rz)
+            rx,ry,rz = R_H1.dir
+            FP=R_H1.dir*self.f/abs(rz)
             d=FP-PI
             if self.f<0:
                     d=-d
+            R=Ray(pos=PI, dir=d, wavelength=wav, n=n)
+            R_H2=R.ch_coord_sys_inv(P,D)
 
-            R3=Ray(pos=PI, dir=d, wavelength=wav, n=n)
-            R4=R3.ch_coord_sys_inv(P,D)
-            R2.add_child(R4)
-
-            ##Propagar hasta C2
-            C,P,D = self.complist["C2"]
-            R=R4.ch_coord_sys(P,D)
+            ##Propagar hasta E2
+            C,P,D = self.complist["E2"]
+            R=R_H2.ch_coord_sys(P,D)
             PI = self.__E2__.intersection(R)
             if self.__E2__.shape.hit(PI):
-                R5=Ray(pos=PI, dir = R.dir, wavelength=wav, n=n)
-                R6=R5.ch_coord_sys_inv(P,D)
-                R4.add_child(R6)
+                R=Ray(pos=PI, dir = R.dir, wavelength=wav, n=n)
+                R_E2=R.ch_coord_sys_inv(P,D)
             else:
-                R4.intensity = 0.
+                R_H2.intensity = 0.
+
+            if self.complete_trace:
+                ri.add_child(R_E1)
+                R_E1.add_child(R_H1)
+                R_H1.add_child(R_H2)
+                if R_H2.intensity !=0:
+                    R_H2.add_child(R_E2)
+            else:
+                R_E1_E2=Ray(pos=R_E1.pos, dir = R_E2.pos-R_E1.pos, wavelength=wav, n=n)
+                ri.add_child(R_E1_E2)
+                if R_H2.intensity !=0:
+                    R_E1_E2.add_child(R_E2)
+                else:
+                    R_E1_E2.intensity = 0
+
         else:
             #codigo a usar si el rayo entra por E2
+            # Es una copia del cvodigo en el if, cambiando el orden.
+            # Hay que buscar una forma mas elegante de hacer esto
+
+            #Encontrar el punto de corte en E2
+            print "Entrando por E2"
+
+            C,P,D = self.complist["E2"]
+            R=ri.ch_coord_sys(P,D)
+            PI=self.__E2__._intersection(R)
+
+            #Generar el rayo que va de E2 a H2
+            if self.complist["E2"][1][2]<self.complist["H2"][1][2]:
+                R = Ray(pos = PI, dir = R.dir,wavelength = wav, n=n)
+            else:
+                R = Ray(pos = PI, dir = -R.dir,wavelength = wav, n=n)
+            R_E2 = R.ch_coord_sys_inv(P,D)
+
             #Propagar hasta  H2
             C,P,D =self.complist["H2"]
-            #Cambiando el rayo al systema de coordenadas de la superficie E1, que
-            #en este caso en particular es el mismo de la componente C1
-            R=ri.ch_coord_sys(P,D)
-            #Calcular el punto de corte con H1 sin verificar apertura
+            R=R_E2.ch_coord_sys(P,D)
+            #Calcular el punto de corte con H2 sin verificar apertura
             PI=self.__H2__._intersection(R)
-            #Crear el rayo que va entre H1 y H2
+            #Crear el rayo que va entre H2 y H1
             if self.complist["H2"][1][2] < self.complist["H1"][1][2]:
-                R1=Ray(pos=PI, dir=(0,0,1), wavelength=wav, n=n)
+                R=Ray(pos=PI, dir=(0,0,1), wavelength=wav, n=n)
             else:
-                R1=Ray(pos=PI, dir=(0,0,-1), wavelength=wav, n=n)
-            R2=R1.ch_coord_sys_inv(P,D)
-            ri.add_child(R2)
+                R=Ray(pos=PI, dir=(0,0,-1), wavelength=wav, n=n)
+            R_H2=R.ch_coord_sys_inv(P,D)
 
             #Propagar hasta H1
             C,P,D =self.complist["H1"]
-            R=R2.ch_coord_sys(P,D)
-            PI=self.__H2__._intersection(R) #No se verifica apertura
+            R=R_H2.ch_coord_sys(P,D)
+            PI=self.__H1__._intersection(R) #No se verifica apertura
 
-            #### Calculate the reffraction in H2
-            rx,ry,rz = ri.dir
-            FP=ri.dir*self.f/abs(rz)
+            #### Calculate the reffraction in H1
+            rx,ry,rz = R_H2.dir
+            FP=R_H2.dir*self.f/abs(rz)
             d=FP-PI
-
             if self.f<0:
                     d=-d
+            R=Ray(pos=PI, dir=d, wavelength=wav, n=n)
+            R_H1=R.ch_coord_sys_inv(P,D)
 
-            R3=Ray(pos=PI, dir=d, wavelength=wav, n=n)
-            R4=R3.ch_coord_sys_inv(P,D)
-            R2.add_child(R4)
-
-            ##Propagar hasta C1
-
-            C,P,D = self.complist["C1"]
-            R=R4.ch_coord_sys(P,D)
-            PI = self.__E2__.intersection(R)
-            if self.__E2__.shape.hit(PI):
-                R5=Ray(pos=PI, dir = R.dir, wavelength=wav, n=n)
-                R6=R5.ch_coord_sys_inv(P,D)
-                R4.add_child(R6)
+            ##Propagar hasta E2
+            C,P,D = self.complist["E1"]
+            R=R_H1.ch_coord_sys(P,D)
+            PI = self.__E1__.intersection(R)
+            if self.__E1__.shape.hit(PI):
+                R=Ray(pos=PI, dir = R.dir, wavelength=wav, n=n)
+                R_E1=R.ch_coord_sys_inv(P,D)
             else:
-                R4.intensity = 0.
+                R_H2.intensity = 0.
+
+            if self.complete_trace:
+                ri.add_child(R_E2)
+                R_E2.add_child(R_H2)
+                R_H2.add_child(R_H1)
+                if R_H1.intensity !=0:
+                    R_H1.add_child(R_E1)
+            else:
+                R_E2_E1=Ray(pos=R_E2.pos, dir = R_E1.pos-R_E2.pos, wavelength=wav, n=n)
+                ri.add_child(R_E2_E1)
+                if R_H1.intensity !=0:
+                    R_E2_E1.add_child(R_E1)
+                else:
+                    R_E2_E1.intensity = 0
 
         return ri
 
@@ -180,7 +214,7 @@ class IdealThickLens(System):
         #print self.complist
 
 
-        for comp  in [self.complist["C1"],self.complist["C2"]]:
+        for comp  in [self.complist["E1"],self.complist["E2"]]:
             C,P,D =comp
             #C,P,D = i
             #Reorientar el rayo, al sistema de coordenadas del elemento
