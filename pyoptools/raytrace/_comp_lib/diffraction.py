c5f3dceaff0cd41fef0153c0a8321787d7aba5eb
Ricardo Amézquita-Orozco <ramezquitao@cihologramas.com>
2017-08-05 19:01:20 -0500

    A diffraction gratting component was added

---
+++ pyoptools/raytrace/_comp_lib/diffraction.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#------------------------------------------------------------------------------
+# Copyright (c) 2007, Ricardo Amézquita Orozco
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the GPLv3
+# license included in LICENSE.txt and may be redistributed only
+# under the conditions described in the aforementioned license.
+#
+#
+# Author:          Ricardo Amézquita Orozco
+# Description:     Spherical lens definitión module
+# Symbols Defined: SphericalLens
+#------------------------------------------------------------------------------
+#
+'''
+Definition of a Diffraction gratting object and helper functions
+'''
+
+from math import pi,sin,cos
+
+from pyoptools.raytrace.component import Component
+from pyoptools.raytrace.surface import Plane, RPPMask
+from pyoptools.raytrace.shape import Rectangular
+from pyoptools.misc.Poly2D.Poly2D import poly2d
+
+
+class RectGratting(Component):
+    def __init__(self,size=(50.,50.,10.), reflectivity = 0., lpmm=600,angle=0,M=[1],*args,**kwargs):
+        Component.__init__(self,*args,**kwargs)
+        self.size=size
+        w, h, l=self.size
+        lpmmx= lpmm*cos(angle)
+        lpmmy= lpmm*sin(angle)
+        phf=poly2d([0,2*pi*lpmmx,2*pi*lpmmy])
+        __a_surf = RPPMask(shape=Rectangular(size=(w, h)), phm = phf, reflectivity = reflectivity,M=M)
+        __p_surf = Plane(shape=Rectangular(size=(w, h)))
+
+        __u_surf = Plane(shape=Rectangular(size=(w, l)))
+        __l_surf = Plane(shape=Rectangular(size=(w, l)))
+
+        __lf_surf = Plane(shape=Rectangular(size=(l, h)))
+        __rg_surf = Plane(shape=Rectangular(size=(l, h)))
+
+        self.surflist["S1"]=(__a_surf,(0,0,0),(0,0,0))
+        self.surflist["S2"]=(__p_surf,(0,0,l),(0,0,0))
+
+        self.surflist["S3"]=(__u_surf,(0,h/2,l/2.),(pi/2,0,0))
+        self.surflist["S4"]=(__l_surf,(0,-h/2,l/2.),(pi/2,0,0))
+
+        self.surflist["S5"]=(__lf_surf,(-w/2,0,l/2.),(0,pi/2,0))
+        self.surflist["S6"]=(__rg_surf,(w/2,0,l/2.),(0,pi/2,0))
--- pyoptools/raytrace/comp_lib.py
+++ pyoptools/raytrace/comp_lib.py
@@ -14,3 +14,4 @@ from ._comp_lib.compound_lens import Doublet, AirSpacedDoublet
 from ._comp_lib.stop import Stop
 from ._comp_lib.ideallens import IdealLens, IdealTLens
 from ._comp_lib.mirror import RoundMirror, RectMirror
+from ._comp_lib.diffraction import RectGratting
