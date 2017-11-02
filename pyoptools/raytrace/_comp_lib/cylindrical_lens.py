93e9b625bef612df676532d6f6c80e8d094aaf49
Ricardo Amézquita-Orozco <ramezquitao@cihologramas.com>
2017-11-02 16:03:05 -0500

    Se adiciona lente cilindrica de apertura rectangular. OJO, no se esta cerrando la lente en los lados

---
+++ pyoptools/raytrace/_comp_lib/cylindrical_lens.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#------------------------------------------------------------------------------
+# Copyright (c) 2007, Ricardo Amézquita Orozco
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the GPLv3
+# license included in LICENSE.txt and may be redistributed only
+# under the conditions described in the aforementioned license.
+#
+#
+# Author:          Ricardo Amézquita Orozco
+# Description:     Spherical lens definitión module
+# Symbols Defined: SphericalLens
+#------------------------------------------------------------------------------
+#
+'''
+Definition of a spherical lens object and helper functions
+'''
+
+#from enthought.traits.api import Float, Instance, HasTraits,  Trait
+from numpy import sqrt, pi, absolute
+
+from pyoptools.raytrace.component import Component
+from pyoptools.raytrace.surface import Cylindrical,  Plane
+from pyoptools.raytrace.shape import Circular,  Rectangular
+class CylindricalLens(Component):
+    ''' **Class to define a cylindrical Lens**.
+    '''
+
+    def __init__(self,size=(20,20), thickness=10, curvature_s1=1./200,curvature_s2=1./200,*args,**kwargs):
+        Component.__init__(self,*args,**kwargs)
+        self.size=size
+        w, h=self.size
+        self.thickness=thickness
+        self.curvature_s1=curvature_s1
+        self.curvature_s2=curvature_s2
+        
+        if self.curvature_s1!=0.:
+            __a_surf= Cylindrical (shape=Rectangular(size=(w, h)),
+                                      curvature=self.curvature_s1)
+        else:
+            __a_surf= Plane(shape=Rectangular(size=(w, h)))
+     
+        if self.curvature_s2!=0:
+            __p_surf= Cylindrical (shape=Rectangular(size=(w, h)),
+                                      curvature=self.curvature_s2)
+        else:
+            __p_surf= Plane(shape=Rectangular(size=(w, h)))
+        
+        self.surflist["S1"]=(__a_surf,(0,0,-self.thickness/2),(0,0,0))
+        self.surflist["S2"]=(__p_surf,(0,0,self.thickness/2 ),(0,0,0))
+
+        #TODO: Falta cerrrar la lente por los lados
--- pyoptools/raytrace/comp_lib.py
+++ pyoptools/raytrace/comp_lib.py
@@ -7,6 +7,7 @@ This module hides from the end user un needed stuff.
 """
 
 from ._comp_lib.spherical_lens import SphericalLens
+from ._comp_lib.cylindrical_lens import CylindricalLens
 from ._comp_lib.ccd import CCD
 from ._comp_lib.prism import RightAnglePrism, PentaPrism, DovePrism
 from ._comp_lib.cube import Block, BeamSplitingCube
