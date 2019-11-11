7d5e3b162d97988b447ac450b3aa8648e46ddee8
Colin Brosseau <colin@colinbrosseau.com>
2019-11-11 00:51:08 -0500

    add test_find_ppp()

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -9,8 +9,9 @@ from pyoptools.raytrace._comp_lib.ccd import CCD
 from pyoptools.raytrace._comp_lib.spherical_lens import SphericalLens
 from pyoptools.raytrace._comp_lib.stop import Stop
 import pyoptools.raytrace.calc.calc as calc
+from pyoptools.raytrace.library import library
 from pyoptools.raytrace.mat_lib import material
-from pyoptools.raytrace.ray import Ray
+from pyoptools.raytrace.ray import Ray, parallel_beam_c
 from pyoptools.raytrace.shape.circular import Circular
 from pyoptools.raytrace.system.system import System
 
@@ -92,3 +93,16 @@ def test_chief_ray_search():
     np.testing.assert_equal(chief_ray.label, "")
     np.testing.assert_equal(chief_ray.orig_surf, None)
     np.testing.assert_almost_equal(chief_ray.order, 0)
+
+
+def test_find_ppp():
+    L1 = library.Edmund.get("45179")  # f=200 r= 25
+    OA = Ray(pos=(0, 0, -10000), dir=(0, 0, 1), wavelength=.55)  # Optical axis
+    C = CCD(size=(10, 10))
+    S = System(complist=[(L1, (0, 0, 100), (0, np.pi, 0)), (C, (0, 0, 320.053), (0, 0, 0))], n=1)
+    PB = parallel_beam_c(origin=(0, 0, 50), direction=(0, 0, 0), size=(15, 15), num_rays=(15, 15), wavelength=.55)
+    S.ray_add(PB)
+    S.propagate()
+
+    result = calc.find_ppp(S, OA)
+    np.testing.assert_almost_equal(result, [0., 0., 104.5670357])
