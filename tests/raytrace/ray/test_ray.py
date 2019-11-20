7b3747f0a54407929d67bbb67789309d42ef496d
Colin Brosseau <colin@colinbrosseau.com>
2019-11-20 00:55:33 -0500

    add tests for Ray

--- tests/raytrace/ray/test_ray.py
+++ tests/raytrace/ray/test_ray.py
@@ -391,6 +391,7 @@ def test_ray_almost_equal_almost_same():
     print(ray2)
     assert ray.Ray.almost_equal(ray1, ray2)
 
+
 def test_ray_almost_equal_less_almost_same():
     "Rays with exactly same attributes are equals."
     wavelength = 0.1234
@@ -421,3 +422,37 @@ def test_ray_almost_equal_less_almost_same():
     print(ray1)
     print(ray2)
     assert not ray.Ray.almost_equal(ray1, ray2)
+
+
+def test_ray_dir():
+    "The direction is always a unitary vector. This is done with the setter"
+    ray1 = ray.Ray(dir=(0, 0, 5))
+    assert np.array_equal(ray1.dir, np.array([0, 0, 1]))
+
+    ray1.dir = (0, 5, -5)
+    np.testing.assert_array_almost_equal(ray1.dir, [0, 0.70710678, -0.70710678])
+
+
+def test_ray_pos():
+    ray1 = ray.Ray()
+
+    ray1.pos = (1, 2, 3)
+    np.testing.assert_array_almost_equal(ray1.pos, [1, 2, 3])
+
+
+def test_copy():
+    ray1 = ray.Ray(
+        pos=np.array([1.0, 0.0, 0.0]),
+        dir=np.array([0.0, 0.0, 1.0]),
+        intensity=1.0,
+        wavelength=0.6328,
+        n=None,
+        label="HeNe",
+        orig_surf=None,
+        order=0,
+    )
+
+    ray2 = ray1.copy()
+
+    assert ray2 == ray1
+
