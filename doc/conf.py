12b8f8f7d3994ceeb18cfa332e2c19dfefe437f0
Ricardo Amézquita-Orozco <ramezquitao@cihologramas.com>
2020-01-27 18:58:01 -0500

    Autodoc test in readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -30,13 +30,13 @@ sys.path.insert(0, os.path.abspath('../'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 #extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig']
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.imgmath', 'sphinx.ext.graphviz',
-              'sphinx.ext.inheritance_diagram','sphinx.ext.todo',
-              'sphinx.ext.coverage', 'sphinx.ext.napoleon',
-              'sphinx.ext.viewcode'] 
+              'sphinx.ext.inheritance_diagram', 'sphinxcontrib.apidoc']
 
-napoleon_google_docstring = False
-napoleon_use_param = False
-napoleon_use_ivar = True
+
+apidoc_module_dir = '../pyoptools'
+apidoc_output_dir = ''
+#apidoc_excluded_paths = ['tests']
+apidoc_separate_modules = True
 
 todo_include_todos = True
 
@@ -208,23 +208,3 @@ latex_documents = [
 
 # If false, no module index is generated.
 #latex_use_modindex = True
-
-
-#def run_apidoc(_):
-#	from sphinx.ext.apidoc import main
-#	import os
-#	import sys
-#	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-#	cur_dir = os.path.abspath(os.path.dirname(__file__))
-#	module = os.path.join(cur_dir,"..","pyoptools")
-#	main(['-e', '-o', cur_dir, module, '--force'])
-
-#def setup(app):
-#	app.connect('builder-inited', run_apidoc)
-
-import sphinx.ext.apidoc
-
-
-def setup(app):
-    #sphinx.ext.apidoc.main(['-f', '-T', '-e', '-o', '.', '../pyoptools'])
-    sphinx.ext.apidoc.main(['-f', '-T', '-e', '-o', 'doc', 'pyoptools'])
