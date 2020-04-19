452ba6889c0ec732131dd8aada28c483c8da926d
GProtoZeroW <string12oil@gmail.com>
2020-04-18 22:36:56 -0600

    scipy.misc.imread -> imageio.imread, see scipy 1.1.0 scipy.misc.imread deprecation notice

--- pyoptools/wavefront/field/field.pyx
+++ pyoptools/wavefront/field/field.pyx
@@ -41,7 +41,9 @@ from scipy.signal import convolve2d, fftconvolve, resample
 from scipy.integrate import simps
 from scipy.interpolate import interp2d,bisplrep,bisplev, griddata
 from scipy.ndimage import map_coordinates
-from scipy.misc import imread
+#depreacted in scipy 1.2.1; change to imageio
+#from scipy.misc import imread
+from imageio import imread
 
 
 #import pp
