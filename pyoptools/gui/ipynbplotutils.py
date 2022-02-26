b052a175486a6e84cf83d32859d4d72bc59aa2e1
ederag <edera@gmx.fr>
2022-02-26 19:30:12 +0100

    disable depth test
    Disabled for correct transparency.

--- pyoptools/gui/ipynbplotutils.py
+++ pyoptools/gui/ipynbplotutils.py
@@ -222,7 +222,7 @@ class Plot3D(object):
         glShadeModel(GL_SMOOTH)  # No parece estar funcionando
         glEnable(GL_LIGHTING)
         glEnable(GL_LIGHT0)
-        glEnable(GL_DEPTH_TEST)
+        glDisable(GL_DEPTH_TEST)
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
