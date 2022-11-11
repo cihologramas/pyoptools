b42eb71f028d9b5bc7bea9603c7fb7f7c46e0e01
rdgraham <richard@rdg.cc>
2022-11-10 19:23:59 -0800

    Tidied up library.

--- pyoptools/raytrace/library/library.py
+++ pyoptools/raytrace/library/library.py
@@ -1,60 +1,111 @@
 from pyoptools.raytrace._comp_lib.optic_factory import optic_factory
 
 import ijson
-from pathlib import Path
-
+import warnings
 import sys
+
 from importlib_resources import files
+from pathlib import Path
 
 # This class overrides the module, to provide direct attribute and item access
 class LibraryModule:
+    """The optics library provides access to optical components from
+    the catalogs of well-known optical suppliers or user-supplied libraries.
+
+    Currently, catalogs from Thorlabs [thorlabs.com] and Edmund
+    [edmundoptics.com] are available.
+
+    Optical elements can be retrieved by part number using dictionary-style
+    access from the library module i.e:
+
+    `library['LB1862']` returns a SphericalLens based on an optic from
+    Thorlabs.
+
+    optionally, the specific supplier catalog can be specified via attribute
+    access. This can be used in the rare case that different catalogs have
+    parts with matching part number, or to speed up lookup time. I.e.
+
+    `library.Thorlabs['LB1862']`
+
+    It is also possible to retrieve a dictionary-descriptor for an optic
+    without instantiating the optical component instance. This is done with
+    the `.descriptor()` method. Descriptors can be used to instantiate
+    optical components using the `component.optic_factory`.
+
+    The library module is iterable using `.items()`. This dictionary-style
+    iterator returns component descriptor. A common usage is to search for
+    optical components matching specific requirements.
+
+    User supplied catalogs can be added using the `.add()` method, which
+    requires the path (string or Pathlib.Path) to a .json optic catalog file.
+    The user supplied catalog can then be accessed by attribute using the
+    given filename.
+    """
 
     def __init__(self):
         self._user_libraries = []
-
-        self.dp = files('pyoptools.raytrace.library').joinpath('catalogs')
+        self.dp = files("pyoptools.raytrace.library").joinpath("catalogs")
 
     def __getattr__(self, name: str):
-        p = self.dp/(name+'.json')
+        p = self.dp / (name + ".json")
         if p.exists():
             return OpticCatalog(p)
         else:
             raise AttributeError(f"Catalog {name} not available in library.")
 
-    def __getitem__(self, name: str):
-        for jf in self._json_files():
-            oc = OpticCatalog(jf)
-            try:
-                return oc[name]
-            except KeyError:
-                continue
-        raise KeyError(f"{name} not found in any available optics catalog.")
-
     def items(self):
         """Generates a dictionary iterator which allows iteration through
         all optical components descriptors in all libraries.
         Note : to instantiate an optical component given a descriptor,
-        use optic_factory(**descrioptor).
+        use component.optic_factory(**descriptor).
         """
         for jfp in self._json_files():
             oc = OpticCatalog(jfp)
             for k, v in oc.items():
                 yield (k, v)
 
-    def get(self, part_number: str):
-        """Depricated method. Simply use dictionay style access to get
+    def parts(self):
+        """Returns a list of all available optical component by part number."""
+        return [k for k, _ in self.items()]
+
+    def descriptor(self, part):
+        """Return the dictionary-descriptor for optic with given part number."""
+        for jf in self._json_files():
+            oc = OpticCatalog(jf)
+            try:
+                return oc.descriptor(part)
+            except KeyError:
+                continue
+        raise KeyError(f"{part} not found in any available optics catalog.")
+
+    def __getitem__(self, part: str):
+        return optic_factory(**self.descriptor(part))
+
+    def get(self, part: str):
+        """Deprecated method. Simply use dictionary style access to get
         an optic given the part number.
         """
-        return optic_factory(**self[part_number])
+        warnings.simplefilter("default")
+        warnings.warn(
+            "This method is deprecated, you can use dictionary-style access" "instead",
+            DeprecationWarning,
+        )
+        return optic_factory(**self.descriptor(part))
 
-    def add(self, userlib: str):
+    def add(self, userlib):
         """Add a user-defined .json file containing optical component
         descriptors to the library.
-        userlib : string path to the .json file.
+        userlib : string path to the .json file or pathlib.Path
         """
-        p = Path(userlib)
 
-        if (p.suffix != '.json' or not p.exists()):
+        if isinstance(userlib, str):
+            p = Path(userlib)
+        elif isinstance(userlib, Path):
+            p = userlib
+        else:
+            raise ValueError(f"{userlib} not a string or Pathlib.Path.")
+
+        if p.suffix != ".json" or not p.exists():
             raise FileNotFoundError(f"{userlib} not a valid .json catalog.")
 
         if p.name in [x.name for x in self._user_libraries]:
@@ -69,23 +120,24 @@ class LibraryModule:
                 self._user_libraries.remove(lib)
 
     def _json_files(self):
-        return list(self.dp.glob('*.json')) + self._user_libraries
+        return list(self.dp.glob("*.json")) + self._user_libraries
+
 
 sys.modules[__name__] = LibraryModule()
+
 
 class OpticCatalog:
     def __init__(self, catalog_path):
         self.catalog_path = catalog_path
-        self.jf = catalog_path.open(mode='r+b')
+        self.jf = catalog_path.open(mode="r+b")
 
     def items(self):
-        gen = ijson.kvitems(self.jf, '', use_float=True)
+        gen = ijson.kvitems(self.jf, "", use_float=True)
         self.jf.seek(0)
         return gen
 
     def descriptor(self, part):
-        """Returns a dictionay-descriptor for a given optical component
-        """
+        """Return the dictionary-descriptor for optic with given part number."""
         results = list(ijson.items(self.jf, part, use_float=True))
         if results:
             self.jf.seek(0)
@@ -97,21 +149,17 @@ class OpticCatalog:
     def __getitem__(self, part):
         return optic_factory(**self.descriptor(part))
 
-    def get(self, part_number):
-        """Depricated method. Simply use dictionay style access to get
-        an optic given the part number.
-        """
-        return optic_factory(**self.descriptor(part_number))
+    def get(self, part):
+        warnings.simplefilter("default")
+        warnings.warn(
+            "This method is deprecated, you can use dictionary-style access" "instead",
+            DeprecationWarning,
+        )
+        return optic_factory(**self.descriptor(part))
 
     def parts(self):
-        """Deprecated method. Iterate using library.items() to get all
-           the available optical components."""
-
-        raise NotImplementedError("Method deprecated, it is now possible "
-                                  "to simply iterate through all optics "
-                                  "descriptors library.items().")
+        """Returns a list of all available optical component by part number."""
+        return [k for k, _ in self.items()]
 
     def __del__(self):
-        #print('closing ', self.jf)
         self.jf.close()
-
--- tests/test_library.py
+++ tests/test_library.py
@@ -1,32 +1,78 @@
 from pyoptools.raytrace.library import library
 from pyoptools.raytrace._comp_lib.optic_factory import optic_factory
+from pyoptools.raytrace._comp_lib.spherical_lens import SphericalLens
 
 import unittest
+import tempfile
 
-class TestAllOptics(unittest.TestCase):
+class TestLibrary(unittest.TestCase):
 
-    #@unittest.skip
+    @unittest.skip
     def test_all_optics(self):
         i = -1
         for part, descriptor in library.items():
             i += 1
-            #print(part)
             with self.subTest(part, i=i):
                 optic = optic_factory(**descriptor)
+
+    def test_direct_access(self):
+
+        lens = library['LB1862']
+        self.assertIsInstance(lens, SphericalLens)
 
     def test_attribute_access(self):
 
         lens = library.Thorlabs['LB1862']
+        self.assertIsInstance(lens, SphericalLens)
+
         lens = library.Edmund['08068']
+        self.assertIsInstance(lens, SphericalLens)
 
     def test_descriptor(self):
 
         self.assertEqual(library.Edmund.descriptor('08068')['radius'], 12.2)
+
+        self.assertEqual(library.descriptor('08068')['radius'], 12.2)
+
         with self.assertRaises(KeyError):
             library.Edmund.descriptor('None')
 
-    #def test_user_lib(self):
+        with self.assertRaises(KeyError):
+            library.descriptor('None')
 
+    def test_parts(self):
+
+        self.assertTrue('LB1862' in library.Thorlabs.parts())
+        self.assertTrue('LB1862' in library.parts())
+
+    def test_user_lib(self):
+
+        ntf = tempfile.NamedTemporaryFile('w', suffix='.json')
+        with ntf as fp:
+            fp.write("""
+               {
+                    "a_test_lens": {
+                        "description": "test",
+                        "material": "BK7",
+                        "glass_catalogs": "SCHOTT",
+                        "thickness": 3.5,
+                        "radius": 12.7,
+                        "curvature_s2": 0.0,
+                        "curvature_s1": 0.02,
+                        "type": "SphericalLens"
+                    }
+                }
+            """)
+            fp.flush()
+            fp.seek(0)
+
+            library.add(ntf.name)
+
+            self.assertEqual(library.descriptor('a_test_lens')['thickness'],
+                             3.5)
+
+            self.assertEqual(library.descriptor('a_test_lens')['description'],
+                             'test')
 
 
 if __name__ == '__main__':
